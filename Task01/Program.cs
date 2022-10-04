ddfdff32e78cffba19344e7b95c05215e1f20fdf
NickDanish <nike19981503@gmail.com>
2022-10-04 21:11:14 +0300

    add Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.Write ("Введите первое число: ");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = square == number * number;
+Console.Write(result);
